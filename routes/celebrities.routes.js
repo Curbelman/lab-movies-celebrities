83a17cf3674b90be56aee7ae5f931b33773cdadd
Miguel Curbelo <mcurbeloj@gmail.com>
2022-11-17 21:43:03 +0100

    adds iteration 3, but cannot load /create page

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+
+router.get("/celebrities/create", (req, res) => {
+    res.render("/celebrities/new-celebrity.hbs");
+  });
+
+router.post("/celebrities/create", (req, res) => {
+    const { name, occupation, catchPhrase } = req.body;
+    Celebrity.create({ name, occupation, catchPhrase })
+    .then(() => {
+        res.redirect('/celebrities');
+    })
+    .catch((err) => {
+        res.render("/celebrities/new-celebrity.hbs");
+    })
+})
 
 module.exports = router;
