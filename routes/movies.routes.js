ddd630ec7e1aab07716f6e724ed07d4a4d921cfe
Miguel Curbelo <mcurbeloj@gmail.com>
2022-11-22 21:01:17 +0100

    adds some hbs files and movie routes

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,3 +1,38 @@
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model")
+
+router.get('/create',(req,res)=>{
+    Celebrity.find()
+    .then(allCelebrities => res.render('movies/new-movie.hbs', {celebrities: allCelebrities}))
+})
+
+router.post('/create',(req,res)=>{
+    const { title, genre, plot, cast } = req.body;
+    Movie.create({title, genre, plot, cast})
+     .then (() => res.redirect('/movies'))
+     .catch(err => console.log(err))
+})
+
+router.get('/', (req,res)=>{
+    Movie.find()
+    .then (foundMovies => res.render('movies/movies.hbs', {movieList: foundMovies}))
+    .catch(err => res.send(err))
+})
+
+router.get('/:id', (req,res) =>{
+    const { id } = req.params;
+    Movie.findById(id)
+    .populate('cast')
+    .then(selectedMovie => res.render('movies/movie-details.hbs', {movie: selectedMovie}))
+    .catch(err => res.send(err))
+})
+
+router.post('/:id/delete', (req, res) => {
+    const { id } = req.params;
+    Movie.findByIdAndRemove(id)
+    .then(deletedFilm => res.redirect('/movies'))
+    .catch(err => res.send(err))
+})
 
 module.exports = router;
